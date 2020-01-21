32a0e517025e9ab9f277efb4a68061b1d3b094f3
Guanyao Wang <guw005@eng.ucsd.edu>
2020-01-21 09:03:40 -0800

    mern

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require('passport');
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
-app.get("/", (req, res) => res.send("Hello a/A!!!"));
+
+// app.get("/", (req, res) => {
+//   const user = new user({
+//     handle: "jim",
+//     email: "jim@jim.jim",
+//     password: "jimisgreat123"
+//   })
+//   user.save()
+//   res.json
+
+
+// })
+
+// app.get("/", (req, res) => {
+//     res.send("Hello a/A!!!");
+// });
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
